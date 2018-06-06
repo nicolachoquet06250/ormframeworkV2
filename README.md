cffd624bdbdc8cda3b414a8956d07e2b713c5665
nicolachoquet06250 <nicolachoquet06250@gmail.com>
2018-06-06 22:12:31 +0200

    Finition de la modularisation des annotations

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ormframeworkV2
-version 2 de ormframework - restructuration
+version 2 de ormframework - restructuration, refactoration, et reconception du framework
--- custom/mvc/models/voici_model.php
+++ custom/mvc/models/voici_model.php
@@ -11,15 +11,8 @@ class voici_model extends Model {
 	 * @route voici/ma_doc
 	 **/
 	public function ma_doc() {
-		PhpDocParser::instence('custom/mvc/models', true);//->model();
-		PhpDocParser::instence()->method();
-		PhpDocParser::instence()->description();
-		PhpDocParser::instence()->httpVerb();
-		PhpDocParser::instence()->params();
-		PhpDocParser::instence()->return();
-		PhpDocParser::instence()->route();
-		PhpDocParser::instence()->to_html('./custom/website/doc/index.html');
-		$response = file_get_contents('./custom/website/doc/index.html');
-		return new Html_view($response);
+        PhpDocParser::instence()->method()->description()->httpVerb()->params()->return()->route();
+
+        return new Html_view(PhpDocParser::instence()->to_html('./custom/website/doc/index.html'));
 	}
 }
